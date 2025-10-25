d5ed63323b549a190161b663ee0889df41c22fd9
Alaa Okasha <mxmmotnt1@gmail.com>
2025-10-26 00:11:41 +0300

    feat: add serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,33 +1,60 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import Category, Post, Comment
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ["id", "username", "email", "first_name", "last_name"]
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            password=validated_data["password"],
+        )
+        return user
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
-
+        fields = ["id", "name", "slug"]
+        read_only_fields = ["slug"]
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
-
+        fields = ["id", "post", "author_name", "content", "created_at"]
+        read_only_fields = ["id", "created_at"]
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    comments = CommentSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
     category_id = serializers.PrimaryKeyRelatedField(
-        queryset=Category.objects.all(), source='category', write_only=True
+        queryset=Category.objects.all(), write_only=True, source="category", required=False, allow_null=True
     )
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'author', 'category', 'category_id', 'comments', 'created_at', 'updated_at']
+        fields = [
+            "id", "title", "slug", "content", "status",
+            "created_at", "updated_at", "published_at",
+            "author", "category", "category_id", "comments"
+        ]
+        read_only_fields = ["slug", "created_at", "updated_at", "published_at", "author"]
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
